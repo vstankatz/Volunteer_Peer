f29ec3f7e24523c7c6a39ec45726cf0622f2831d
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-10 09:57:52 -0800

    add methods to pass second spec

--- lib/project.rb
+++ lib/project.rb
@@ -13,9 +13,18 @@ def self.all
   returned_projects.each do |project|
     title = project.fetch("title")
     id = project.fetch("id")
-    projects.push(Project({:title => title, :id => id}))
+    projects.push(Project.new({:title => title, :id => id}))
   end
   projects
+end
+
+def save
+  new_project = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id = new_project.first.fetch("id").to_i
+end
+
+def ==(project_to_compare)
+  self.title.downcase.eql?(project_to_compare.title.downcase)
 end
 
 

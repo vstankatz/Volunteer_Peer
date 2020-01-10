02bd021e7ea9baf619172957949078be22232188
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-10 11:35:07 -0800

    adds save and == method for Volunteer class, passing specs

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -17,9 +17,19 @@ def self.all
     name = volunteer.fetch("name")
     project_id = volunteer.fetch("project_id")
     id = volunteer.fetch("id")
-    volunteers.push(Volunteer.new({:new => name, :project_id => project_id, :id => id}))
+    volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
   end
   volunteers
 end
+
+def save
+   new_volunteer = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id.to_i}) RETURNING id;")
+   @id = new_volunteer.first.fetch("id").to_i
+end
+
+def ==(volunteer_to_compare)
+  (self.name.downcase.eql?(volunteer_to_compare.name.downcase))&&(self.project_id.to_i.eql?(volunteer_to_compare.project_id.to_i))
+end
+
 
 end

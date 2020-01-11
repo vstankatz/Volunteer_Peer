d38e43556153bf27af71afe85e6da6399713e1fe
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-10 16:20:23 -0800

    add more views.

--- app.rb
+++ app.rb
@@ -30,3 +30,46 @@ get('/home/admin') do
   erb(:admin_projects)
 end
 #Admin view of project list
+get('/home/admin/new') do
+  erb(:admin_new)
+end
+
+post('/home/admin') do
+  project = Project.new({:title => params[:proj_title], :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:admin_projects)
+end
+
+get('/home/admin/:proj_id/volunteer') do
+  @project = Project.find(params[:proj_id])
+  erb(:admin_project)
+end
+
+get('/home/volunteer') do
+  @volunteers = Volunteer.all
+  erb(:admin_volunteers)
+end
+
+get('/home/admin/:proj_id/volunteer/new') do
+@project = Project.find(params[:proj_id])
+  erb(:new_volunteer)
+end
+
+post('/home/admin/:proj_id/volunteer') do
+  @project = Project.find(params[:proj_id])
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save
+  erb(:thanks)
+end
+
+get('/home/admin/:proj_id/volunteer/:id') do
+  @project = Project.find(params[:proj_id])
+  @volunteer = Volunteer.find(params[:id])
+  erb(:thanks)
+end
+
+delete('/home/admin/:proj_id/volunteer/:id') do
+  @project = Project.find(params[:proj_id])
+
+end
